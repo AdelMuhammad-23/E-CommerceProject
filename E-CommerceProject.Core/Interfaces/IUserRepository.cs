8a96a966cde3829fba31b2105a1cad529e3049b9
Adel Muhammad <141270987+AdelMohamed23@users.noreply.github.com>
2024-11-22 19:50:35 +0200

    Add Register Endpoint

--- E-CommerceProject.Core/Interfaces/IUserRepository.cs
+++ E-CommerceProject.Core/Interfaces/IUserRepository.cs
@@ -1,6 +1,9 @@
-﻿//namespace E_CommerceProject.Core.Interfaces
-//{
-//    public interface IUserRepository
-//    {
-//    }
-//}
+﻿using E_CommerceProject.Core.Entities.Identity;
+
+namespace E_CommerceProject.Core.Interfaces
+{
+    public interface IUserRepository
+    {
+        public Task<string> AddUserAsync(User user, string password);
+    }
+}
