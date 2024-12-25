04182fa6b5f7aec0e97aae44159c0151c4d1dde9
Adel Muhammad <141270987+AdelMohamed23@users.noreply.github.com>
2024-12-25 20:33:59 +0200

    Add DeleteReview Endpoint Add Get All Reviews EndPoint

--- E-CommerceProject.Infrastrucure/Repositories/ReviewRepository.cs
+++ E-CommerceProject.Infrastrucure/Repositories/ReviewRepository.cs
@@ -1,4 +1,6 @@
-﻿using E_CommerceProject.Core.Entities;
+﻿using E_CommerceProject.Core.DTOs.Pagination;
+using E_CommerceProject.Core.DTOs.ReviewsDTOs;
+using E_CommerceProject.Core.Entities;
 using E_CommerceProject.Core.Interfaces;
 using E_CommerceProject.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
@@ -18,6 +20,41 @@ namespace E_CommerceProject.Infrastructure.Repositories
             await _reviews.AddAsync(addReview);
             await _dbContext.SaveChangesAsync();
             return "Success";
+        }
+
+        public async Task<string> DeleteReviewAsync(Review deleteReview)
+        {
+            _reviews.Remove(deleteReview);
+            await _dbContext.SaveChangesAsync();
+            return "Success";
+        }
+
+        public async Task<PaginatedResult<ReviewsPaginatedResultDTO>> GetPaginationReviews(int pageNumber = 1, int pageSize = 10)
+        {
+            var query = _reviews.AsQueryable();
+
+            var totalItems = await query.CountAsync();
+
+            var reviews = await query
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .Select(r => new ReviewsPaginatedResultDTO
+                {
+                    ReviewId = r.ReviewId,
+                    UserId = r.UserId,
+                    ProductId = r.ProductId,
+                    Rating = r.Rating.Value,
+                    Comment = r.Comment
+                })
+                .ToListAsync();
+            return new PaginatedResult<ReviewsPaginatedResultDTO>
+            {
+                Items = reviews,
+                PageNumber = pageNumber,
+                PageSize = pageSize,
+                TotalItems = totalItems,
+                TotalPages = (int)Math.Ceiling(totalItems / (double)pageSize)
+            };
         }
 
         public async Task<string> UpdateReviewAsync(Review updateReview)
